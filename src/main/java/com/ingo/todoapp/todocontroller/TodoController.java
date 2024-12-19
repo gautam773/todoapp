a61af5655165cffea139a4d975421145fb4d98f6
java-springboot-projects <arjunjaishankar5525@gmail.com>
2024-12-19 22:58:34 +0530

    10th commit

--- src/main/java/com/ingo/todoapp/todocontroller/TodoController.java
+++ src/main/java/com/ingo/todoapp/todocontroller/TodoController.java
@@ -33,8 +33,8 @@ public class TodoController {
         return new ResponseEntity<>("Todo created Successfully",HttpStatus.OK);
     }
 
-    @PostMapping("removeTodo")
-    public ResponseEntity<String> removeTodo(@RequestParam Long id)
+    @DeleteMapping("removeTodo/{id}")
+    public ResponseEntity<String> removeTodo(@PathVariable Long id)
     {
         todoService.removeTodo(id);
         return new ResponseEntity<>("Sucessfully Removed",HttpStatus.OK);
